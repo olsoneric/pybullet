7d21f411f98309e158b1b185f62a56a2b64f9f2f
Eric Olson <git@olsoneric.com>
2017-03-16 07:30:53 -0500

    Python3-style prints

--- bullet/memory.py
+++ bullet/memory.py
@@ -17,12 +17,12 @@ def measure(n, params):
     type, args = params[0], params[1:]
     type(*args)
     before = memused()
-    print type.__name__, before,
+    print(type.__name__, before,)
     sys.stdout.flush()
     for i in n:
         type(*args)
     after = memused()
-    print after, after - before
+    print(after, after - before)
 
 
 
